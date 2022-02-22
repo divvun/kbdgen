888f803519b3f50b6aa097378cddc4bcc1e591ba
alvskar <olga@technocreatives.com>
2022-02-22 12:48:16 +0100

    Add more things, reference the old yaml fork in horror

--- src/bundle/mod.rs
+++ src/bundle/mod.rs
@@ -4,8 +4,17 @@ pub struct KbdgenBundle {
 
 }
 
-pub fn read_kbdgen_bundle(path: &Path) -> KbdgenBundle {
-    KbdgenBundle {
+pub fn read_kbdgen_bundle(path: &Path) -> Result<KbdgenBundle, Error> {
+    Ok(KbdgenBundle {
 
-    }
+    })
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("{0}")]
+    YamlError(#[from] serde_yaml::Error),
 }
