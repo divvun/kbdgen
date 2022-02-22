3ff0db7ec54c3d679cb37e84ff222f18c1bbb58e
alvskar <olga@technocreatives.com>
2022-02-22 12:56:15 +0100

    paaths

---
+++ README.md
@@ -0,0 +1,5 @@
+# kbdgen 2/3.0
+
+This is a developer oriented Readme.
+
+`cargo run -- target ~/Projects/keyboard-sme svg`
--- src/bundle/mod.rs
+++ src/bundle/mod.rs
@@ -1,10 +1,16 @@
-use std::path::Path;
+use std::fs::canonicalize;
+use std::path::{Path, PathBuf};
 
 pub struct KbdgenBundle {
 
 }
 
 pub fn read_kbdgen_bundle(path: &Path) -> Result<KbdgenBundle, Error> {
+
+    let canonical_bundle_path: PathBuf = canonicalize(path)?;
+
+    println!("Canonical Bundle Path: {:?}", &canonical_bundle_path);
+
     Ok(KbdgenBundle {
 
     })
