433021c82af1b5eb022b817c04dcb359f302cb71
alvskar <olga@technocreatives.com>
2022-02-22 11:23:53 +0100

    Some basic things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
 fn main() {
+    let cli = Cli::parse();
+
+    
+
     println!("Hello, world!");
+}
+
+#[derive(Parser)]
+#[clap(version)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    #[clap(subcommand)]
+    Target(TargetCommand)
+}
+
+#[derive(Subcommand)]
+enum TargetCommand {
+    Svg(TargetSvgCommand)
+}
+
+#[derive(Parser)]
+struct TargetSvgCommand {
+    #[clap(short, long)]
+    hmm: bool,
 }
