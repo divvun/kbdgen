713c08d5145503d3c1347601919f90a33961d3d9
alvskar <olga@technocreatives.com>
2022-03-01 17:42:49 +0100

    adding space

--- src/bundle/layout/mod.rs
+++ src/bundle/layout/mod.rs
@@ -1,5 +1,5 @@
 use indexmap::IndexMap;
-use serde::{Deserialize, Serialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize};
 
 use android::{AndroidKbdLayerKey, AndroidPlatformKey};
 use chrome::{ChromeKbdLayerKey, ChromePlatformKey};
@@ -22,25 +22,38 @@ pub struct Layout {
 
     #[serde(default, deserialize_with = "from_mapped_sequence")]
     pub longpress: Option<IndexMap<String, Vec<String>>>,
+
+    pub space: Option<Space>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Layers {
     windows: Option<IndexMap<WindowsPlatformKey, IndexMap<WindowsKbdLayerKey, String>>>,
     chrome: Option<IndexMap<ChromePlatformKey, IndexMap<ChromeKbdLayerKey, String>>>,
+    #[serde(rename = "macos")]
     macOS: Option<IndexMap<MacOSPlatformKey, IndexMap<MacOSKbdLayerKey, String>>>,
     #[serde(rename = "ios")]
     iOS: Option<IndexMap<iOSPlatformKey, IndexMap<iOSKbdLayerKey, String>>>,
     android: Option<IndexMap<AndroidPlatformKey, IndexMap<AndroidKbdLayerKey, String>>>,
 }
 
-fn from_mapped_sequence<'de, D>(deserializer: D) -> Result<Option<IndexMap<String, Vec<String>>>, D::Error>
+fn from_mapped_sequence<'de, D>(
+    deserializer: D,
+) -> Result<Option<IndexMap<String, Vec<String>>>, D::Error>
 where
     D: Deserializer<'de>,
 {
     let map: IndexMap<String, String> = Deserialize::deserialize(deserializer)?;
-    
-    Ok(Some(map.into_iter().map(|(key, value)| {
-        (key, value.split(" ").map(|s| s.to_owned()).collect())
-    }).collect()))
+
+    Ok(Some(
+        map.into_iter()
+            .map(|(key, value)| (key, value.split(" ").map(|s| s.to_owned()).collect()))
+            .collect(),
+    ))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Space {
+    #[serde(rename = "macos")]
+    macOS: Option<IndexMap<MacOSKbdLayerKey, String>>,
 }
