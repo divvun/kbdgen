04e3c6839efe1056228b638018a613b70a7cea8a
alvskar <olga@technocreatives.com>
2022-05-09 12:00:15 +0200

    Add some files

--- src/build/android/generate_android.rs
+++ src/build/android/generate_android.rs
@@ -2,9 +2,13 @@ use std::path::Path;
 use std::str::FromStr;
 
 use async_trait::async_trait;
+use language_tags::LanguageTag;
 use xmlem::{Document, Selector};
 
-use crate::{build::BuildStep, bundle::{KbdgenBundle, layout::android::AndroidKbdLayer}};
+use crate::{
+    build::BuildStep,
+    bundle::{layout::android::AndroidKbdLayer, KbdgenBundle},
+};
 
 const ROWKEYS_TEMPLATE: &str = include_str!("../../../resources/template-android-rowkeys.xml");
 
@@ -13,41 +17,73 @@ const RESOURCES_PART: &str = "res";
 const MAIN_XML_PART: &str = "xml";
 const SHORT_WIDTH_XML_PART: &str = "xml-sw600dp";
 
+const DEFAULT_LOCALE: &str = "en";
+
 pub struct GenerateAndroid;
 
 #[async_trait(?Send)]
 impl BuildStep for GenerateAndroid {
     async fn build(&self, bundle: &KbdgenBundle, output_path: &Path) {
+        let main_xml_path = output_path
+            .join(Path::new(TOP_FOLDER))
+            .join(Path::new(RESOURCES_PART))
+            .join(Path::new(MAIN_XML_PART));
 
-        let main_xml_path = Path::new(TOP_FOLDER).join(Path::new(RESOURCES_PART)).join(Path::new(MAIN_XML_PART));
+        std::fs::create_dir_all(&main_xml_path).unwrap();
 
+        let default_language_tag =
+            LanguageTag::parse(DEFAULT_LOCALE).expect("default language tag must parse");
 
-
-        // One rowkeys_{displayName}_keyboard{count}.xml file per language with an Android platform 
+        // One rowkeys_{displayName}_keyboard{count}.xml file per language with an Android platform
         // (pretending we're following the primary approach for start)
         for (language_tag, layout) in &bundle.layouts {
             if let Some(android_target) = &layout.android {
+                let rowkeys_display_name = layout
+                    .display_names
+                    .get(&default_language_tag)
+                    .expect(&format!("no '{}' displayName!", DEFAULT_LOCALE))
+                    .to_lowercase();
 
                 let layers = &android_target.primary.layers;
 
-
-
-                let mut rowkeys_document = Document::from_str(ROWKEYS_TEMPLATE).expect("invalid template");
+                let rowkeys_document =
+                    Document::from_str(ROWKEYS_TEMPLATE).expect("invalid template");
                 let rowkeys_root = rowkeys_document.root();
 
-                let selector = Selector::new("merge.switch.case").unwrap();
-
-
                 if let Some(default_layer) = layers.get(&AndroidKbdLayer::Default) {
+                    let selector = Selector::new("switch").unwrap();
 
+                    for (line_index, line) in default_layer.lines().enumerate() {
+                        let new_rowkeys_document = rowkeys_document.clone();
+                        let new_rowkeys_root = new_rowkeys_document.root();
 
-                    let default_row_keys = rowkeys_root
-                        .query_selector(&rowkeys_document, &selector)
-                        .expect("The template document should have a tag at 'merge.switch.case'");
+                        let default_row_keys = new_rowkeys_root
+                            .query_selector(&new_rowkeys_document, &selector)
+                            .expect("The template document should have a switch tag");
 
+                        let selector = Selector::new("case").unwrap();
 
-                    
+                        let default_row_keys = new_rowkeys_root
+                            .query_selector(&new_rowkeys_document, &selector)
+                            .expect("The template document should have a case tag");
 
+                        std::fs::write(
+                            main_xml_path.join(format!(
+                                "rowkeys_{}_keyboard{}.xml",
+                                rowkeys_display_name,
+                                line_index + 1
+                            )),
+                            new_rowkeys_document.to_string_pretty(),
+                        )
+                        .unwrap();
+                    }
+
+                    // take the keymap layer and first split by lines
+                    // each line is xml document
+                    // and there should be 3 lines
+
+                    // keyhintlabel seems hardcoded 1 to 0
+                    // if run out of hardcoded keyhintlabel just drop it
 
                     /*
                     default_row_keys.append_new_element(
@@ -61,19 +97,12 @@ impl BuildStep for GenerateAndroid {
                         },
                     );
                      */
-
-
-
-                } else { // maybe just hardcode
+                } else {
+                    // maybe just hardcode
                     panic!("No default layer for android!")
                 }
             }
         }
-
-
-
-
-
 
         // Files added for kbd-sme (confirm)
 
@@ -83,7 +112,6 @@ impl BuildStep for GenerateAndroid {
 
         // json file name: {layout}.json
 
-
         let jni_libs = "jniLibs"; // join top
         let arm = "arm64-v8a"; // join jni
         let other_arm = "armeabi-v7a"; // join jni
@@ -91,30 +119,21 @@ impl BuildStep for GenerateAndroid {
         let jni_file_1 = "libdivvunspell.so"; // join arm
         let jni_file_2 = "libpahkat_client.so"; // join arm
 
-
-
         let res_folder = "res"; // join top
 
-
-
         let top_values = "values"; // join res
-        // top values folder. (non-critical, ignore initially)
-        // modify the 'strings-appname.xml' to make sure it has the
-        // appropriate keyboard display names
-
-
+                                   // top values folder. (non-critical, ignore initially)
+                                   // modify the 'strings-appname.xml' to make sure it has the
+                                   // appropriate keyboard display names
 
         let xml_folder1 = "xml"; // join res
         let xml_folder2 = "xml-sw600dp"; // join res. Do we support other screen ranges?
-
-
 
         // xml is the core folder that also contains the root linking xml file
         // seems we add a file with name:
 
         // rowkeys_{displayName}_keyboard{count}.xml
         // displayName seems to be the   en: Northern Sami displayname
-
 
         /*
           (use "git add <file>..." to include in what will be committed)
