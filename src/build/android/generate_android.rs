d128f3dfb0e1c6e64ace39b16857cfdbf23af1f7
alvskar <olga@technocreatives.com>
2022-05-09 10:43:19 +0200

    blap

--- src/build/android/generate_android.rs
+++ src/build/android/generate_android.rs
@@ -4,12 +4,79 @@ use async_trait::async_trait;
 
 use crate::{build::BuildStep, bundle::KbdgenBundle};
 
+const ROWKEYS_TEMPLATE: &str = include_str!("../../../resources/template-android-rowkeys.xml");
+
+const TOP_FOLDER: &str = "app/src/main";
+const RESOURCES_PART: &str = "res";
+const MAIN_XML_PART: &str = "xml";
+const SHORT_WIDTH_XML_PART: &str = "xml-sw600dp";
+
 pub struct GenerateAndroid;
 
 #[async_trait(?Send)]
 impl BuildStep for GenerateAndroid {
     async fn build(&self, bundle: &KbdgenBundle, output_path: &Path) {
+
+        let main_xml_path = Path::new(TOP_FOLDER).join(Path::new(RESOURCES_PART)).join(Path::new(MAIN_XML_PART));
+
+
+
+        // One rowkeys_{displayName}_keyboard{count}.xml file per language with an Android platform 
+        // (pretending we're following the primary approach for start)
+        for (language_tag, layout) in &bundle.layouts {
+            if let Some(android_target) = &layout.android {
+
+                let layers = &android_target.primary.layers;
+
+
+            }
+        }
+
+
+
+
+
+
         // Files added for kbd-sme (confirm)
+
+        let top_folder = "app/src/main";
+
+        let json_folder_join = "assets/layouts"; // join top
+
+        // json file name: {layout}.json
+
+
+        let jni_libs = "jniLibs"; // join top
+        let arm = "arm64-v8a"; // join jni
+        let other_arm = "armeabi-v7a"; // join jni
+
+        let jni_file_1 = "libdivvunspell.so"; // join arm
+        let jni_file_2 = "libpahkat_client.so"; // join arm
+
+
+
+        let res_folder = "res"; // join top
+
+
+
+        let top_values = "values"; // join res
+        // top values folder. (non-critical, ignore initially)
+        // modify the 'strings-appname.xml' to make sure it has the
+        // appropriate keyboard display names
+
+
+
+        let xml_folder1 = "xml"; // join res
+        let xml_folder2 = "xml-sw600dp"; // join res. Do we support other screen ranges?
+
+
+
+        // xml is the core folder that also contains the root linking xml file
+        // seems we add a file with name:
+
+        // rowkeys_{displayName}_keyboard{count}.xml
+        // displayName seems to be the   en: Northern Sami displayname
+
 
         /*
           (use "git add <file>..." to include in what will be committed)
@@ -45,6 +112,7 @@ impl BuildStep for GenerateAndroid {
         // app/src/main/res/values-sv/strings.xml -> these seem to be based on display name
         // entries
         // subtle changes
+        // change in xml namespace that brendon said isn't super valid
 
         // modified app/src/main/res/values/strings-appname.xml
 
