5634d9163ea1c9733e295f7aa8a1a67c976c3504
Brendan Molloy <brendan@necessary.nu>
2025-04-22 17:04:23 +0200

    Add reversed build lines from msklc

--- src/build/windows/build_klc.rs
+++ src/build/windows/build_klc.rs
@@ -76,3 +76,124 @@ impl KlcBuildTarget {
         }
     }
 }
+
+fn cl_command(include_path: &str, source_path: &str) -> std::process::Command {
+    let mut cmd = std::process::Command::new("cl.exe");
+    cmd.arg("-nologo")
+        .arg(format!("-I{}", include_path))
+        .arg("-DNODGICAPMASKS")
+        .arg("-DNO_WIN_MESSAGES")
+        .arg("-DNO_WIN_STYLES")
+        .arg("-DNO_SYSMETRICS")
+        .arg("-DNOMENUS")
+        .arg("-DNOCIONS")
+        .arg("-DNOSYSCOMMANDS")
+        .arg("-DNORASTEROPS")
+        .arg("-DNOSHOWWINDOW")
+        .arg("-DOEMRESOURCE")
+        .arg("-DONATOM")
+        .arg("-DNOCURSOR")
+        .arg("-DNOCOLOR")
+        .arg("-DNOCTLMGR")
+        .arg("-DNODRAWTEXT")
+        .arg("-DNOGDI")
+        .arg("-DNOKERNEL")
+        .arg("-DNONLS")
+        .arg("-DNOMB")
+        .arg("-DNOMEMMGR")
+        .arg("-DNOMETAFILE")
+        .arg("-DNOMINMAX")
+        .arg("-DNOMSG")
+        .arg("-DNOOPENFILE")
+        .arg("-DNOSCROLL")
+        .arg("-DNOSERVICE")
+        .arg("-DNOSOUND")
+        .arg("-DNOTEXTMETRIC")
+        .arg("-DNOWINOFFSETS")
+        .arg("-DNOWH")
+        .arg("-DNOCOMM")
+        .arg("-DNOKANJI")
+        .arg("-DJI")
+        .arg("-DNOHELP")
+        .arg("-DNOPROFILER")
+        .arg("-DNODEFERWINDOWPOS")
+        .arg("-DNOMCX")
+        .arg("-DWIN32_LEAN_AND_MEAN")
+        .arg("-Droster")
+        .arg("-DSTDCALL")
+        .arg("-D_WIN32_W")
+        .arg("-DNT=0x0500")
+        .arg("/DWINVER=0x0500")
+        .arg("-D_WIN32_IE=0x0500")
+        .arg("/MD")
+        .arg("/c")
+        .arg("/Zp8")
+        .arg("/Gy")
+        .arg("/W3")
+        .arg("/WX")
+        .arg("/Gz")
+        .arg("/Gm-")
+        .arg("/EHs-c-")
+        .arg("/GR-")
+        .arg("/GF")
+        .arg("-Z7")
+        .arg("/Oxs")
+        .arg(source_path)
+        .arg(format!("{}.c", source_path));
+    cmd
+}
+
+fn rc_command(include_path: &str, resource_path: &str) -> std::process::Command {
+    let mut cmd = std::process::Command::new("rc.exe");
+    cmd.arg("-r")
+        .arg(format!("-I{}", include_path))
+        .arg("-DSTDCALL")
+        .arg("-DCONDITION_HANDLING=1")
+        .arg("-DNT_UP=1")
+        .arg("-DNT_INST=0")
+        .arg("-DWIN32=100")
+        .arg("-D_NT1X_=100")
+        .arg("-DWINNT=1")
+        .arg("-D_WIN32_WINNT=0x0500")
+        .arg("/DWINVER=0x0400")
+        .arg("-D_WIN32_IE=0x0400")
+        .arg("-DWIN32_LEAN_AND_MEAN=1")
+        .arg("-DDEVEL=1")
+        .arg("-DFPO=1")
+        .arg("-DNDEBUG")
+        .arg("-l")
+        .arg("409")
+        .arg(format!("{}.rc", resource_path));
+    cmd
+}
+
+fn link_command(
+    obj_path: &str,
+    lib_path: &str,
+    def_path: &str,
+    res_path: &str,
+) -> std::process::Command {
+    let mut cmd = std::process::Command::new("link.exe");
+    cmd.arg("-nologo")
+        .arg(obj_path)
+        .arg(lib_path)
+        .arg("-SECTION:INIT,D")
+        .arg("-OPT:REF")
+        .arg("-OPT:ICF")
+        .arg("-IGNORE:4039,4078")
+        .arg("-noentry")
+        .arg("-dll")
+        .arg(format!("-libpath:{}", lib_path))
+        .arg("-subsystem:native,5.0")
+        .arg("-merge:.rdata,.text")
+        .arg("-PDBPATH:NONE")
+        .arg("-STACK:0x40000,0x1000")
+        .arg("/opt:nowin98")
+        .arg("-osversion:4.0")
+        .arg("-version:4.0")
+        .arg("/release")
+        .arg(format!("-def:{}.def", def_path))
+        .arg(format!("{}.res", res_path))
+        .arg(format!("{}.obj", obj_path));
+    cmd
+}
